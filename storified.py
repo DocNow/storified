00c0bbaf3ed9ed8941f81fedc50f6091723f638a
Ed Summers <ehs@pobox.com>
2017-12-15 15:11:56 -0500

    added --download-dir option for control over where the data is downloaded to

--- storified.py
+++ storified.py
@@ -5,18 +5,26 @@ import logging
 import argparse
 import requests
 
-def storified(account_name):
+def storified(account_name, archive_dir=None):
     try:
-        archive_dir = setup_dir(account_name)
-        logging.basicConfig(
-            filename=os.path.join(archive_dir, "storified.log"),
-            level=logging.INFO
-        )
+        if archive_dir:
+            archive_dir = setup_dir(archive_dir)
+        else:
+            archive_dir = setup_dir(account_name)
+
+        setup_logging(archive_dir)
+        logging.info("starting to archive stories for %s to %s", account_name,
+            archive_dir)
+
         for story in get_stories(account_name):
             archive_story(story, archive_dir)
+
         logging.info("finished archiving account %s", account_name)
+
     except StorifiedException as e:
-        logging.error(e)
+        print(e)
+        if len(logging.getLogger().handlers) != 0:
+            logging.error(e)
 
 def get_stories(account_name):
     page = 0
@@ -63,12 +71,19 @@ def rewrite_html(story_dir):
 
 def setup_dir(path):
     if (os.path.exists(path)):
-        raise StorifiedException("directory %s already exists", path)
+        raise StorifiedException("directory %s already exists" % path)
     try:
         os.mkdir(path)
     except Exception as e:
         raise StorifiedException("unable to create target directory: %s", path)
     return path
+
+def setup_logging(archive_dir):
+    logging.basicConfig(
+        filename=os.path.join(archive_dir, 'storified.log'),
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s"
+    )
 
 class StorifiedException(Exception):
     pass
@@ -76,5 +91,7 @@ class StorifiedException(Exception):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("archive your Storify stories")
     parser.add_argument("account_name", help="Your Storify account name")
+    parser.add_argument("--download-dir", "-d", default=None, 
+                        help="a directory to download data to")
     args = parser.parse_args()
-    storified(args.account_name)
+    storified(args.account_name, args.download_dir)
